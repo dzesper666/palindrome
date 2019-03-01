02387ceb1399c87f8e635c9673014b75f718305f
ksarama <ksarama@amctech.pl>
2019-03-01 13:13:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Wojtek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool palindrom()
+            {
+                Console.WriteLine("Wprowadź tekst:");
+                string kacper = Console.ReadLine();
+
+                char[] kacperDoTablicy = kacper.ToCharArray();
+
+                Array.Reverse(kacperDoTablicy);
+
+                string kacperWstringu = new String(kacperDoTablicy);
+
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Twój teskt po odwróceniu wygląda tak: ");
+                Console.WriteLine(kacperWstringu);
+                System.Threading.Thread.Sleep(2000);
+
+                if (kacper == kacperWstringu)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Więc:");
+                    Console.WriteLine("TAK - wprowadzony przez Ciebie tekst jest palindromem");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Więc:");
+                    Console.WriteLine("NIE - wprowadzony przez Ciebie tekst nie jest palindromem");
+                    return false;
+                }
+            }
+
+            palindrom();
+
+        }
+    }
+}
+
+
+
